079d3695d90f2189d9206e74c12af069d1492ebe
xrr2016 <13286904669@163.com>
2017-01-05 23:48:11 +0800

    :zap: finish Day-29

--- 29-CountDown Clock/script.js
+++ 29-CountDown Clock/script.js
@@ -1,10 +1,15 @@
 let countdown
 const timerDisplay = document.querySelector('.display__time-left')
+const endTimeDisplay = document.querySelector('.display__end-time')
+const buttons = document.querySelectorAll('[data-time]')
+
 
 function timer(seconds){
+  clearInterval(countdown)
   const now = Date.now()
   const then = now + seconds * 1000
   displayTimeLeft(seconds)
+  displayEndTime(then)
   countdown = setInterval(()=>{
     const secondsLeft = Math.round((then - Date.now()) / 1000)
     // console.log(secondsLeft)
@@ -23,3 +28,21 @@ function displayTimeLeft(seconds){
   document.title = display
   timerDisplay.textContent = display
 }
+
+function displayEndTime(timestamp){
+  const end = new Date(timestamp)
+  const hours = end.getHours()
+  const minutes = end.getMinutes()
+  endTimeDisplay.textContent = `Be Back At ${hours} : ${minutes < 10 ?"0":""} ${minutes}`
+}
+buttons.forEach(button => {
+  button.addEventListener('click',function(){
+    timer(parseInt(this.dataset.time))
+  })
+})
+document.customForm.addEventListener('submit',function(e){
+  e.preventDefault()
+  const minutes = this.minutes.value
+  timer(minutes * 60)
+  this.reset()
+})
